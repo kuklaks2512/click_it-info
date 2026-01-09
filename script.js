2f93d7e09024ed29824642d1aafd81f1ca03015c
kuklaks2512 <kuklaks2512@gmail.com>
2026-01-09 18:11:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 document.getElementById('playBtn').addEventListener('click', () => {
-    window.open('http://127.0.0.1:5500/game%20itself/index.html');
+    window.open('https://kuklaks2512.github.io/click_it-game/');
 });
 
 document.getElementById('subscribeForm').addEventListener('submit', (e) => {
     e.preventDefault();
     alert('Дякуємо за підписку!');
     e.target.reset();
+
 });
