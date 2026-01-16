5e9cd3ee07e5b57845cb9a8225b2c9be6e85f0e1
kuklaks2512 <kuklaks2512@gmail.com>
2026-01-16 18:01:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+alert('Please enter fullscreen mode (F11)');
+
+
 document.getElementById('playBtn').addEventListener('click', () => {
     window.open('https://kuklaks2512.github.io/click_it-game/');
 });
@@ -6,5 +10,4 @@ document.getElementById('subscribeForm').addEventListener('submit', (e) => {
     e.preventDefault();
     alert('Дякуємо за підписку!');
     e.target.reset();
-
 });
